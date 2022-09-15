5cac8ae5e3f3e89be9c47036eb2a357d5e35b9b9
Daniel Kramer <47264875+drk98@users.noreply.github.com>
2022-09-15 15:44:05 -0700

    Added setup files (Still not ready to work)

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+import os
+from setuptools import setup, find_packages, dist
+
+
+import numpy as np
+
+requirements = [
+    "numpy",
+]
+
+
+
+setup(
+    setup_requires=[
+        # Setuptools 18.0 properly handles Cython extensions.
+        "setuptools>=18.0",
+        "numpy",
+    ],
+    author="Daniel Kramer",
+    author_email="drk98@nau.edu",
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    description="A Python package for running LS on a GPU",
+    install_requires=requirements,
+    license="MIT license",
+    include_package_data=True,
+    keywords="GPULS",
+    name="GPULS",
+    packages=find_packages(include=["source", "source.*"]),
+    url="https://github.dev/SNAPS-Alert-Broker",
+    version='1.0.0',
+    zip_safe=False,
+    ext_modules=modules,
+    include_dirs=[np.get_include()],
+    extra_compile_args=["-O3"],
+)
